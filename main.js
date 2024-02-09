71b5abd0650bf3f6f0731c8502a5e44dd5f417b8
Victor Hernandez <victorhernandez1101@gmail.com>
2024-02-08 18:44:19 -0800

    Second commit, fixed a run error

--- main.js
+++ main.js
@@ -2,20 +2,19 @@ const { app, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 
-
 function createMainWindow() {
-    const MainWindow = new BrowserWindow({
-        title: "Processing Log",
+    const mainWindow = new BrowserWindow({
+        title: 'Processing Log',
         width: 1000,
         height: 600,
     });
 
     const startUrl = url.format({
         pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file',
+        protocol: 'file'
     });
 
-    MainWindow.loadURL(startUrl);
+    mainWindow.loadURL(startUrl);
 }
 
-app.whenReady().then(createMainWindow);
+app.on('ready', createMainWindow);
