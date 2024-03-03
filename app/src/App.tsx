3abb002dd0f743a0fa41ed807807bc8b777d29b6
Victor Hernandez <victorhernandez1101@gmail.com>
2024-03-02 18:47:44 -0800

    tried a couple of things that didn't work

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import CreatableSelect from 'react-select/creatable';
 import { options } from './components/options';
